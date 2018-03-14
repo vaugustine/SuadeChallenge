98f6b38065878842ab0fea046669813c9397a03c
vaugustine <v.vargheseaugustine@gmail.com>
2018-03-14 13:15:59 +0000

    added error handlers to the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 
 from app.models.models import db
 from app.apis.report import api
@@ -16,7 +16,14 @@ def create_app(config_module=None):
 	def index():
 		response = make_response('Suade Challenge by vaugustine !!!')
 		response.headers['Content-Type'] = 'text/plain'
-
 		return response
+
+	@app.errorhandler(404)
+	def not_found_error(error):
+		return jsonify({'error_code':404, 'error_desc':'Not Found'}), 404
+
+	@app.errorhandler(500)
+	def not_found_error(error):
+		return jsonify({'error_code':500, 'error_desc':'Internal Server Error'}), 500
 
 	return app
